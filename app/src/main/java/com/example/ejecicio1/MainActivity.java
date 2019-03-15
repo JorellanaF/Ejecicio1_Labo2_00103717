814a262f74f15767449d10aa4eb6b68790e0b6c7
JorellanaF <00103717@uca.edu.sv>
2019-03-15 10:41:44 -0600

    Muestra user y password

--- app/src/main/java/com/example/ejecicio1/MainActivity.java
+++ app/src/main/java/com/example/ejecicio1/MainActivity.java
@@ -2,12 +2,43 @@ package com.example.ejecicio1;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText username;
+    private EditText password;
+    private Button btn1;
+    private TextView text;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        username = findViewById(R.id.username);
+        password = findViewById(R.id.password);
+        btn1 = findViewById(R.id.button1);
+        text = findViewById(R.id.text1);
+
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                text.setText(username.getText());
+            }
+        });
+
+        btn1.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View v) {
+                btn1.setText("LongClick");
+                text.setText(password.getText());
+                return false;
+            }
+        });
     }
 }
